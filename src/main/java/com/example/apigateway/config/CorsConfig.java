f306f21810f7f8dc4e8770b99429ca13bd131d9f
jmk445 <jmk445123456@gmail.com>
2025-07-03 16:21:16 +0900

    fix : cors allow

---
+++ src/main/java/com/example/apigateway/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://15.164.81.45"); // 프론트 도메인
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // 또는 "GET", "POST" 등 명시적으로 설정
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
