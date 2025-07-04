8f5d36aa8d30df929ef2eb84fc0555cce1177766
jmk445 <jmk445123456@gmail.com>
2025-07-04 10:56:46 +0900

    feat : deploy

--- src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ApiGatewayApplicationTests {
-
+	//test코드가 모두 통과 했을 때 github actions가 배포를 한다.
+	String str1 = "Hello";
 	@Test
-	void contextLoads() {
+	void test(){
+		assertEquals(str1,"Hello");
 	}
 
 }
