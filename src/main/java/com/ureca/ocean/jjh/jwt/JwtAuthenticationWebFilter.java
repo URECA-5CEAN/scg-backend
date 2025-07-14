1f8c7d46e8be38f587444c0e12c60171ef76c8cd
jmk445 <jmk445123456@gmail.com>
2025-07-14 15:33:23 +0900

    feat : user email을 header에 넣어서 redirect

--- src/main/java/com/ureca/ocean/jjh/jwt/JwtAuthenticationWebFilter.java
+++ src/main/java/com/ureca/ocean/jjh/jwt/JwtAuthenticationWebFilter.java
@@ -3,6 +3,7 @@ package com.ureca.ocean.jjh.jwt;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
@@ -28,12 +29,29 @@ public class JwtAuthenticationWebFilter implements WebFilter {
 		String token = exchange.getRequest().getHeaders().getFirst("Authorization");
 		log.info("token : " + token);
 		if (token != null && jwtUtil.validateToken(token)) {
-			String username = jwtUtil.getUsernameFromToken(token);
+			String username = jwtUtil.getUsernameFromToken(token); //username의 실제 값은 auth-backend에서 email을 넣어서 보내줌.
 
 			Authentication auth = new UsernamePasswordAuthenticationToken(username, null, List.of());
 			SecurityContext context = new SecurityContextImpl(auth);
-			return chain.filter(exchange)
+
+
+			//userId를 microservice로 넘기기 전
+//			return chain.filter(exchange)
+//					.contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(context)));
+
+			//userId를 microservice로 넘기기 후
+			ServerHttpRequest mutatedRequest = exchange.getRequest()
+					.mutate()
+					.header("X-User-email", username)
+					.build();
+
+			ServerWebExchange mutatedExchange = exchange.mutate()
+					.request(mutatedRequest)
+					.build();
+
+			return chain.filter(mutatedExchange)
 					.contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(context)));
+
 		}
 
 		return chain.filter(exchange);
