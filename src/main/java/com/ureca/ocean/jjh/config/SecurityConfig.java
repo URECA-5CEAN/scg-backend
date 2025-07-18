7e655f8e0f19d93129232f1a5507af5faf89070e
jmk445 <jmk445123456@gmail.com>
2025-07-18 20:01:16 +0900

    feat : api/map은 허용

--- src/main/java/com/ureca/ocean/jjh/config/SecurityConfig.java
+++ src/main/java/com/ureca/ocean/jjh/config/SecurityConfig.java
@@ -1,23 +1,21 @@
 package com.ureca.ocean.jjh.config;
 
-
-
 import com.ureca.ocean.jjh.jwt.JwtAuthenticationWebFilter;
 import com.ureca.ocean.jjh.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final JwtUtil jwtUtil;
     private final MyAuthenticationEntryPoint entryPoint;
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
@@ -35,17 +33,18 @@ public class SecurityConfig {
                                 "/api-docs/swagger-config",
                                 "/api-ui.html",
                                 "/webjars/**",
-                                "/api/store",
                                 "/api/auth/login",
                                 "/api/user/signup",
                                 "/api/auth/v3/api-docs",
                                 "/api/user/v3/api-docs",
                                 "/api/ai/v3/api-docs",
-                                "/api/map/v3/api-docs"
+                                "/api/map/v3/api-docs",
+                                "/api/map/store",
+                                "/api/map/store/*"
                         ).permitAll()
                         .anyExchange().authenticated()
                 )
-                .addFilterAt(new JwtAuthenticationWebFilter(jwtUtil),  SecurityWebFiltersOrder.AUTHENTICATION)
+                .addFilterAt(new JwtAuthenticationWebFilter(jwtUtil), SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
     }
 }
