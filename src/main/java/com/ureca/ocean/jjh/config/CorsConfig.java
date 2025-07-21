0cac1a9b08e2193311e5b572d29d8a168dd4bd3a
jmk445 <jmk445123456@gmail.com>
2025-07-21 15:33:33 +0900

    chore : 127.0.0.1도 추ê¸가

--- src/main/java/com/ureca/ocean/jjh/config/CorsConfig.java
+++ src/main/java/com/ureca/ocean/jjh/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://15.164.81.45","http://localhost:5173")); // 여기에 프론트엔드 주소 추가
+        config.setAllowedOrigins(List.of("http://15.164.81.45","http://localhost:5173","http://127.0.0.1:5173"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); //get, post, put, delete 모두
 
