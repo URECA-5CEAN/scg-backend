3d3a595b96d53fe33c0e954fb79a7d1174732156
jmk445 <jmk445123456@gmail.com>
2025-07-21 15:30:16 +0900

    feat : cors 오류 해결

--- src/main/java/com/ureca/ocean/jjh/config/CorsConfig.java
+++ src/main/java/com/ureca/ocean/jjh/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
@@ -13,7 +15,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.setAllowedOrigins(List.of("http://15.164.81.45","http://localhost:5173")); // 여기에 프론트엔드 주소 추가
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); //get, post, put, delete 모두
 
