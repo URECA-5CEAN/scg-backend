04b8d2f637c610eba4b7cff6ca35db4271039964
jmk445 <jmk445123456@gmail.com>
2025-08-20 17:46:44 +0900

    feat : eureka 적용 준비 완료

--- src/main/java/com/ureca/ocean/jjh/config/RouteLocatorConfig.java
+++ src/main/java/com/ureca/ocean/jjh/config/RouteLocatorConfig.java
@@ -16,27 +16,25 @@ public class RouteLocatorConfig {
                         .path("/ws/chat")
                         .uri("ws://10.0.2.41:8082")) // WebSocket 서버 주소
 
-//                // HTTP 요청: /api/ws/chat/info/** 는 http 프로토콜로
-//                .route("ws_chat_info_route", r -> r.path("/api/ws/chat/info/**")
+                //eureka 적용 전 라우팅
+//                .route("auth_route", r -> r.path("/api/auth/**")
+//                        .uri("http://10.0.1.17:8081"))
+//                .route("map_route", r -> r.path("/api/map/**")
+//                        .uri("http://10.0.1.239:8082"))
+//                .route("ai_route", r -> r.path("/api/ai/**")
+//                        .uri("http://10.0.1.141:8082"))
+//                .route("user_route", r -> r.path("/api/user/**")
 //                        .uri("http://10.0.2.41:8082"))
-//
-//                // WebSocket 요청: /api/ws/chat/** 는 ws 프로토콜로
-//                // (단, /api/ws/chat/info/** 경로와 겹치지 않도록 주의)
-//                .route("ws_chat_ws_route", r -> r
-//                        .path("/api/ws/chat/**")
-//                        .and()
-//                        .predicate(exchange -> !exchange.getRequest().getURI().getPath().startsWith("/api/ws/chat/info"))
-//                        .uri("ws://10.0.2.41:8082"))
 
-                // 기존 다른 라우팅들
+                //eureka 적용 후 라우팅
                 .route("auth_route", r -> r.path("/api/auth/**")
-                        .uri("http://10.0.1.17:8081"))
+                        .uri("lb://auth-backend"))
                 .route("map_route", r -> r.path("/api/map/**")
-                        .uri("http://10.0.1.239:8082"))
+                        .uri("lb://map-backend"))
                 .route("ai_route", r -> r.path("/api/ai/**")
-                        .uri("http://10.0.1.141:8082"))
+                        .uri("lb://ai-backend"))
                 .route("user_route", r -> r.path("/api/user/**")
-                        .uri("http://10.0.2.41:8082"))
+                        .uri("lb://user-backend"))
 
                 .build();
     }
